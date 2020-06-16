753e58fb184c5ca8627b15a8f5d79d73d221ee21
chiclasico <fundoshi.chiclasico@gmail.com>
2020-06-16 11:30:16 +0900

    処理方法変更

--- src/main/java/org/embulk/decoder/unzip/UnzipInputStream.java
+++ src/main/java/org/embulk/decoder/unzip/UnzipInputStream.java
@@ -1,39 +1,54 @@
 package org.embulk.decoder.unzip;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
-import org.apache.commons.compress.archivers.ArchiveException;
-import org.apache.commons.compress.archivers.ArchiveInputStream;
-import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class UnzipInputStream extends InputStream {
 
 	private InputStream is;
+	private BufferedInputStream bis;
+	private ZipInputStream zis;
 	
 	public UnzipInputStream(InputStream is) {
-		this.is = is;
+   		this.is = is;
+   		this.bis = new BufferedInputStream(is);
+   		this.zis = new ZipInputStream(bis, StandardCharsets.UTF_8);
 	}
 
 	@Override
+	public void close() throws IOException {
+		zis.close();
+		bis.close();
+		is.close();
+	}
+	
+	@Override
 	public int read() throws IOException {
 
-		ArchiveInputStream in = null;
-		try {
-			in = new ArchiveStreamFactory().createArchiveInputStream(ArchiveStreamFactory.ZIP, is);
-		} catch (ArchiveException e) {
-			e.printStackTrace();
-		}
-		return in.read();
+		ZipEntry zipEntry = zis.getNextEntry();
+		StringBuilder sb = new StringBuilder();
 
-//		ZipArchiveEntry entry = (ZipArchiveEntry)in.getNextEntry();
-//		entry.
-//		OutputStream out = Files.newOutputStream(dir.toPath().resolve(entry.getName()));
-//		IOUtils.copy(in, out);
-//		out.close();
-//		in.close();
+		if (zipEntry != null) {
+			System.out.println(String.format("Entry: %s len %d", zipEntry.getName(), zipEntry.getSize()));
 
+	        BufferedReader br = new BufferedReader(new InputStreamReader(zis, "UTF-8"));
+	        String line;
+	        while ((line = br.readLine()) != null) {
+	        	sb.append(line);
+	        	System.out.println(line);
+	        }
+	        br.close();
+	    } else 
+	    	return -1;
+		
+		return new ByteArrayInputStream(sb.toString().getBytes("utf-8")).read();
 	}
-
+	
 }
